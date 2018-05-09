c137a135afd0672c79bbc07e91c243565d42a982
Maciej Klimowski <maciek.klimowski44@gmail.com>
2018-05-09 09:44:42 +0200

    Added Bootstrap and added demo home/index

--- StoreDemo/Controllers/HomeController.cs
+++ StoreDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StoreDemo.ViewModels;
 
 namespace StoreDemo.Controllers
 {
@@ -11,7 +12,18 @@ namespace StoreDemo.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var model = new BaseSearchDepartmentsViewModel
+            {
+                DepartmentsNames = new List<string>()
+                {
+                    "First",
+                    "Second",
+                    "Third",
+                    "Fourth"
+                }
+            };
+            
+            return View(model);
         }
     }
 }
---
+++ StoreDemo/ViewModels/BaseSearchDepartmentsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreDemo.ViewModels
+{
+    public class BaseSearchDepartmentsViewModel
+    {
+        public List<string> DepartmentsNames { get; set; }
+    }
+}
