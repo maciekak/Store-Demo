f5a5138bbb8e2e3124867f769269d80abec81bad
Maciej Klimowski <maciek.klimowski44@gmail.com>
2018-05-09 18:36:47 +0200

    Added some css and js

---
+++ StoreDemo/Scripts/scripts.js
@@ -0,0 +1,10 @@
+﻿var flag = true;
+$('#header').on('click', function () {
+    if (flag) {
+        $('#header').addClass('black');
+        flag = false;
+    } else {
+        $('#header').removeClass('black');
+        flag = true;
+    }
+})
